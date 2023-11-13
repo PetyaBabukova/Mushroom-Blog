909cdad82aa3a5009fe3680fd397687d2d81e1a1
Petya Babukova <p_babukova@abv.bg>
2023-11-13 12:05:03 +0200

    Adjust some small things

--- client/src/components/DishDetails/DishDetails.jsx
+++ client/src/components/DishDetails/DishDetails.jsx
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom';
 import * as dishService from '../../services/dishService';
 
 function DishDetails() {
+
     const [dish, setDish] = useState({});
     const {id} = useParams()
     console.log(id);
@@ -12,27 +13,30 @@ function DishDetails() {
         dishService.getOne(id)
             .then(searchedDish=>setDish(searchedDish[0]))
             console.log(dish);
-           
-    }, [id])
+    }, [id]);
+
+    const editBtnClickHandler = ()=>{
+      
+    }
 
     return (
         <div className={styles.dishDetailsContainer}>
             <div className={styles.imageContainer}>
-        <img src={dish.image} alt='Chef Image' />
+        <img src={dish.image} alt='Dish Image' />
       </div>
       <div className={styles.details}>
         <h2 className={styles.dishTitle}> {dish.title} </h2>
         <p className={styles.ingradients}> <span className={styles.ingradientsWord}>Ingradients: </span>{dish.ingredients}</p>
         <p className={styles.ingradients}><span className={styles.ingradientsWord}>Instructions: </span>{dish.description}</p>
-        <p className={styles.ingradients}><span className={styles.ingradientsWord}>Optional: </span>{dish.optionalExtras}</p>
-        <h4>{dish.author}</h4>
+        <h4 className={styles.author}>{dish.author}</h4>
         <div className={styles.actions}>
-          <button className={styles.chefProfileBtn}>edit</button>
-          <button className={styles.chefProfileBtn}>delete</button>
-          <button className={styles.chefProfileBtn}>like</button>
+          <button className={styles.actionBtn}>Create</button>
+          <button className={styles.actionBtn}>Edit</button>
+          <button className={styles.actionBtn}>Delete</button>
+          <button className={styles.actionBtn}>Like</button>
         </div>
-        <div className={styles.footer}>
-          {/* <span className={styles.rating}>Rating: {dish.rating}</span> */}
+        <div className={styles.rating}>
+          <span className={styles.ratingContent}>Likes: {dish.rating}</span>
           {/* <span className={styles.dishes}>Dishes</span> */}
         </div>
       </div>
