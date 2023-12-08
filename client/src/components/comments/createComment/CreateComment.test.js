98c4503a29facc90cdd975d763e808923aa22bb1
Petya Babukova <p_babukova@abv.bg>
2023-12-08 12:59:09 +0200

    Instal and config mocha/chai and babel

---
+++ client/src/components/comments/createComment/CreateComment.test.js
@@ -0,0 +1,8 @@
+// src/components/YourComponent.test.js
+import { expect } from 'chai';
+
+describe('YourComponent', () => {
+  it('should do something', () => {
+    expect(true).to.be.true;
+  });
+});
---
+++ client/test/setup.js
@@ -0,0 +1,4 @@
+require('jsdom-global')();
+global.expect = require('chai').expect;
+global.assert = require('chai').assert;
+// other global variables that your tests might need
